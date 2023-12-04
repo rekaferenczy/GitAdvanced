dc82a9e1098899e6a242f77be4fc8db99813b397
Reka Ferenczy <ferenczy.reka01@gmail.com>
2023-12-04 15:16:21 +0100

    a5

--- GitAdvanced/src/ZoliAge.java
+++ GitAdvanced/src/ZoliAge.java
@@ -1,5 +1,5 @@
 public class ZoliAge {
-    public int count (int month) {
-        return month;
+    public String count (int month, String name) {
+        return name + "was born in" + month;
     }
 }
